56d2b4dcadb9894636153e657761afea8cbbe8a7
Christine Lao <clao@ucsd.edu>
2015-02-27 09:29:35 +0000

    analytics lab

--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -1,5 +1,15 @@
 'use strict';
 
+
+(function(){
+        var t,i,e,n=window,o=document,a=arguments,s="script",r=["config","track","identify","visit","push","call","trackForm","trackClick"],c=function(){var t,i=this;for(i._e=[],t=0;r.length>t;t++)(function(t){i[t]=function(){return i._e.push([t].concat(Array.prototype.slice.call(arguments,0))),i}})(r[t])};for(n._w=n._w||{},t=0;a.length>t;t++)n._w[a[t]]=n[a[t]]=n[a[t]]||new c;i=o.createElement(s),i.async=1,i.src="//static.woopra.com/js/w.js",e=o.getElementsByTagName(s)[0],e.parentNode.insertBefore(i,e)
+})("woopra");
+
+woopra.config({
+    domain: 'chlao-lab8.herokuapp.com'
+});
+woopra.track();
+
 // Call this function when the page loads (the "ready" event)
 $(document).ready(function() {
 	initializePage();
@@ -13,10 +23,12 @@ function initializePage() {
 
   $(".version_a").click(function(){
     //add your Woopra tracking code for version A's like button click event
+    woopra.track("a_version_like_click"); 
   })
 
   $(".version_b").click(function(){
     //add your Woopra tracking code for version A's like button click event
+    woopra.track("b_version_like_click");   
   })
 
 }
